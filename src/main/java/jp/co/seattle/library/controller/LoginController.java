97016bcdfa3080f8d491657ea52547f34a73647a
nana-okubo <nana.okubo@seattleconsulting.co.jp>
2021-04-20 16:19:28 +0900

    タスク3完了

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.co.seattle.library.dto.UserInfo;
 import jp.co.seattle.library.service.BooksService;
 import jp.co.seattle.library.service.UsersService;
 
@@ -45,9 +46,14 @@ public class LoginController {
 
         // TODO 下記のコメントアウトを外してサービスクラスを使用してください。
 //        UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
-
-        // TODO パスワードとメールアドレスの組み合わせ存在チェック実装
-        
+        UserInfo selectedUserInfo = usersService.selectUserInfo(email,password);
+        // TODO パスワードとメールアドレスの組み合わせ存在チェック実装        
+                if (selectedUserInfo == null) {
+            model.addAttribute("errorMessage","アカウントが存在しません");
+            return "login";
+               
+        }
+      
 
 
         // 本の情報を取得して画面側に渡す
